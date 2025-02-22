3a5d608858abc30e8825161efa68832d9c98dd2a
ceciliareynoso <cecilia.reynosom@outlook.com>
2025-02-22 09:40:27 -0500

    elimina datos de prueba de la tabla usuarios en DDL.sql

--- backend/database/DDL.sql
+++ backend/database/DDL.sql
@@ -15,11 +15,3 @@ CREATE TABLE usuarios (
   PRIMARY KEY (id)
 );
 
--- Insertar datos de prueba
-INSERT INTO usuarios (email, password, nombre, direccion, rol) VALUES
-('comprador@example.com', '$2a$10$4bLWAX0XA7CAl4aTsStW0ulkFO1A.be4mvEB.GWjTddxeAMt8UDhy', 'Empleado Comprador', 'Dirección PetsCareSolutions', 'COMPRADOR');
-
-INSERT INTO usuarios (email, password, nombre, direccion, rol) VALUES
-('admin@example.com', '$2a$10$NlLPvl3fKD6Jz/fynEFZOebnQ.klUB6IFbwHZ.vGCES3UPeyf6Rfm', 'Admin', 'Admin Address', 'ADMINISTRADOR');
-INSERT INTO usuarios (email, password, nombre, direccion, rol) VALUES
-('vendedor@example.com', '$2a$10$8yUn/M7uoBMIL6AO2/Mnl.3vdB.uA954O/e3OnHuwtPJCmnAcnomC', 'Empleado Vendedor', 'Dirección PetsCareSolutions of 2', 'VENDEDOR');
