3d789124a49b809daf70fd059bbcdcf58c8c297f
ceciliareynoso <cecilia.reynosom@outlook.com>
2025-02-23 11:44:59 -0500

    elimina la configuración de CORS condicional en index.js, estableciendo siempre la URL de producción

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin:
-    process.env.NODE_ENV === 'production' 
-    ? 'https://psc-ecommerce-dl.onrender.com'
-    : 'http://localhost:5173',
- 
+    'https://psc-ecommerce-dl.onrender.com'
 })
 );
 // app.use(cors());
