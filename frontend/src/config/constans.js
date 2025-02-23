35b7dc7946f03ea15a5a481e6be790ce1eb69407
ceciliareynoso <cecilia.reynosom@outlook.com>
2025-02-23 11:00:44 -0500

    actualiza URLBASE a http://localhost:3000 en constans.js

--- frontend/src/config/constans.js
+++ frontend/src/config/constans.js
@@ -1,5 +1,5 @@
 // export const URLBASE = 'https://psc-ecommerce-dl-backend.onrender.com';
-export const URLBASE = "http://localhost:10000"
+export const URLBASE = "http://localhost:3000";
 
 export const ENDPOINT = {
   // Endpoints públicos
